ac10429479f29eeac2a097a482b2b968b14e1292
Diego + Esop <Diego+Esop@appacademy.io>
2014-05-12 11:17:06 -0400

    Create Tile methods

--- Minesweeper.rb
+++ Minesweeper.rb
@@ -1,9 +1,32 @@
 class Tile
   
+  DELTAS = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [-1,1], [-1,-1], [1,-1]]
+  
+  # attr_reader :bombed
+  attr_accessor :flagged :revealed
+  
   def initialize(pos)
     @bombed = false
     @flagged = false
     @revealed = false
   end
+  
+  def bombed?
+    @bombed
+  end
+  
+  def neighbors(pos)
+    DELTAS.map {|x,y| [x+pos[0],y+pos[1]]}
+  end
+  
+  def neighbor_bomb_count
+    count = 0
+    neighbors.each {|neighbor| count += 1 if neighbor.bombed? }
+    count
+  end
+  
+  
+  
+
   
 end
